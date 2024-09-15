908f34fbcae12db16dce47e0467cfa31c32e5df4
Pankaj <pankajnarayan317@gmail.com>
2024-09-15 20:31:51 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";  // require('dotenv').config({path: './env'})
 import connectDB from "./db/index.js";
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: './env' });
 
 // Check environment variables
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
       statusCode,
       message="Something went wrong",
       errors =[],
-      stack=""
+      statck=""
     ){
         super(message)
         this.statusCode = statusCode
@@ -12,7 +12,7 @@ class ApiError extends Error {
         this.success=false
         this.errors = errors
 
-        if(stack){
+        if(statck){
             this.stack=stack
         }else{
             Error.captureStackTrace(this,this.constructor)
