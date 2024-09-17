a4ae780dc498ac0e0dbadaea7b3f5b3520010534
Pankaj <pankajnarayan317@gmail.com>
2024-09-17 22:11:13 +0530

    all set up configured

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
-import express from 'express'
-import cors from'cors'
-import cookieParser from'cookie-parser'
-const app=express();
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
 
 app.use(cors({
-    orgin:process.env.CORS_ORIGIN,
-    credentials:true
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
 
-app.use(express.json({
-    limit:"16kb"
-}))
-
-app.use(express.urlencoded({
-    extended:true,
-    limit:"16kb"
-}))
-
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
 app.use(cookieParser())
 
-app.use(express.static("public"))
-export {app}
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+export { app }
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,10 @@
-class ApiRespoonse {
-    constructor (statusCode,data,message="Success"){
-        this.statusCode = statusCode,
-        this.data=data,
-        this.message=message,
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
         this.success = statusCode < 400
     }
 }
+
+export { ApiResponse }
